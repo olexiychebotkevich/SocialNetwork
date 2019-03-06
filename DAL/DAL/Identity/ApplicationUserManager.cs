246143c3bfb9e7f0d2c9978e62d1f9daa386b69c
olexiychebotkevich <olexiychebotkevich@A33COMP4>
2019-03-06 17:42:27 +0200

    DAL

---
+++ DAL/DAL/Identity/ApplicationUserManager.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+                : base(store)
+        {
+        }
+    }
+}
